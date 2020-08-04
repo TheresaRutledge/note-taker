aca30c878b2d8ee3992199f21514ca9c7943e2bd
Theresa Rutledge <t.roserutledge@gmail.com>
2020-08-04 13:30:02 -0700

    new note pushes to db.json

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
-const app = express();
+const PORT = process.env.PORT || 3001;
 const {notes} = require('./db/db.json');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 
 app.get('/api/notes',(req, res) => {
-    res.send(notes);
-})
+    res.json(notes);
+});
 
-app.listen(3001,()=> {
-    console.log('Server now on 3001');
+app.post('/api/notes',(req,res) =>{
+    saveToNotes(req.body);
+    res.send(notes)
+});
+
+
+saveToNotes=(newNote)=>{
+    notes.push(newNote);
+}
+
+app.listen(PORT,()=> {
+    console.log(`Server now on ${PORT}`);
 })
