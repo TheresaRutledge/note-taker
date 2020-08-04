354b03c77c80f341363b8f66caada0e45e10200f
Theresa Rutledge <t.roserutledge@gmail.com>
2020-08-04 14:31:55 -0700

    added index and notes routes

--- server.js
+++ server.js
@@ -6,21 +6,25 @@ const { networkInterfaces } = require('os');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 const fs = require('fs');
 const path = require('path');
+const e = require('express');
 
 //API to access existing notes
 app.get('/api/notes',(req, res) => {
-    console.log(notes);
     res.json(notes);
 });
 
 //API to create new note
 app.post('/api/notes',(req,res) =>{
-    console.log(notes);
+  if(req.body.title){
     saveToNotes(req.body,notes);
     res.send(notes)
+  } else {
+      res.status(400).send('Must give note a title');
+  }
 });
 
 //saves new note to db.json object
@@ -31,6 +35,15 @@ saveToNotes=(newNote, notesArray)=>{
     fs.writeFileSync(path.join(__dirname,'./db/db.json'),JSON.stringify({notes: notesArray}),null,2);
     return networkInterfaces;
 }
+//route to landing page
+app.get('/',(req,res)=> {
+    res.sendFile(path.join(__dirname,'./public/index.html'))
+});
+
+// route to notes page
+app.get('/notes',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/notes.html'));
+})
 
 app.listen(PORT,()=> {
     console.log(`Server now on ${PORT}`);
